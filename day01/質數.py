7779b9a70678172c417584b0944b4df0ef2db702
keepmyfavor <keepmyfavor@gmail.com>
2021-05-06 15:45:22 +0800

    20210506-1600

--- day01/質數.py
+++ day01/質數.py
@@ -1,5 +1,3 @@
-# 印出 2~100 間的質數
-
 def is_prime(n):
     for i in range(2, n):
         if n % i == 0:
@@ -31,26 +29,3 @@ def main():
 
 
 main()
-
-
-"""
-
-# 使用者輸入數值
-num = int(input("請輸入一個數值: "))
- 
-# 質數大於 1
-if num > 1:
-   # 檢視因子
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"不是質數")
-           print(i,"乘於",num//i,"是",num)
-           break
-   else:
-       print(num,"是質數")
-       
-# 如果輸入的數值小於或等於 1，不是質數
-else:
-   print(num,"不是質數")
-
-"""
